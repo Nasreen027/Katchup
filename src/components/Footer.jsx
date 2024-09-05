75794ed24c3efd2ea253a5a1babc5633b0e9e480
Nasreen <nasreenmalik429@gmail.com>
2024-09-05 20:31:40 +0500

    validation added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,21 @@
-import { Box,Text } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Text } from '@chakra-ui/react';
+import React from 'react';
 
 const Footer = () => {
   return (
-    <>
-      <Box borderTop={'1px solid #e1d6c5'}>
-        <Text textAlign={'center'}>© All Rights Reserved | Aurora | Developed by Nasreen.
-        </Text>
-      </Box>
-    </>
-  )
-}
+    <Box
+      position="fixed"
+      bottom="0"
+      width="100%"
+      borderTop="1px solid #e1d6c5"
+      py={4}
+      bg="#fff"
+    >
+      <Text textAlign="center" color="black">
+        © All Rights Reserved | Aurora | Developed by Nasreen.
+      </Text>
+    </Box>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,104 +17,140 @@ import { Link } from "react-router-dom";
 import { UnAuthenticatedRoutesNames } from "../utilities/util.const";
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
-    const LoginFormSubmitHandler = (e) => {
-        e.preventDefault();
-        if(!email|| !password){
-            return
-        }
-        console.log(email, 'email', password, 'password');
-    };
+  const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
 
-    return (
-        <Flex minH="100vh" align="center" justify="center" bg={colors.bg.primary}>
-            <Box
-                w="100%"
-                maxW="lg"
-                p={8}
-                borderRadius="md"
-                boxShadow="lg"
-                position="relative"
-                overflow="hidden"
-            >
-                <Heading mb={8} textAlign="center" color={colors.text.primary}>
-                    Login
-                </Heading>
-                <form onSubmit={LoginFormSubmitHandler}>
-                    <VStack spacing={5}>
-                        <InputGroup
-                            icon={customIcons.envelope}
-                            placeholder="Email"
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <InputGroup
-                            icon={customIcons.lock}
-                            placeholder="Password"
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </VStack>
-                    <Button
-                        mt={8}
-                        w="full"
-                        bg={colors.bg.accent}
-                        color={colors.text.secondary}
-                        _hover={{ bg: "rgba(102, 0, 51, 0.75)" }}
-                        size="lg"
-                        type="submit"
-                    >
-                        Login
-                    </Button>
-                </form>
+  const LoginFormSubmitHandler = (e) => {
+    e.preventDefault();
+    let isValid = true;
 
-                <Divider my={8} borderColor={colors.text.secondary} />
+    if (!email) {
+      setEmailError("Email is required!");
+      isValid = false;
+    } else if (!validateEmail(email)) {
+      setEmailError("Please enter a valid email address!");
+      isValid = false;
+    } else {
+      setEmailError("");
+    }
+    if (!password) {
+      setPasswordError("Password is required");
+      isValid = false;
+    } else if (password.length < 6) {
+      setPasswordError("Password must be atleast six characters long");
+      isValid=false;
+    } else {
+      setPasswordError("");
+    }
+    if (isValid) {
+      console.log(email, "email", password, "password");
+    }
+  };
 
-                <Text textAlign="center" color={colors.text.primary}>
-                    Don't have an account?{" "}
-                    <Link
-                        to={UnAuthenticatedRoutesNames.Register}
-                        fontWeight={"bold"}
-                        as="span"
-                        color={colors.bg.accent}
-                        cursor="pointer"
-                    >
-                        Create one
-                    </Link>
-                </Text>
-            </Box>
-        </Flex>
-    );
+  return (
+    <Flex minH="100vh" align="center" justify="center" bg={colors.bg.primary}>
+      <Box
+        w="100%"
+        maxW="lg"
+        p={8}
+        borderRadius="md"
+        boxShadow="lg"
+        position="relative"
+        overflow="hidden"
+      >
+        <Heading mb={8} textAlign="center" color={colors.text.primary}>
+          Login
+        </Heading>
+        <form onSubmit={LoginFormSubmitHandler}>
+          <VStack spacing={5}>
+            <InputGroup
+              icon={customIcons.envelope}
+              placeholder="Email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {emailError && (
+              <Text fontWeight={'bold'} color="red.500" fontSize="sm" textAlign="left" w="full">
+                {emailError}
+              </Text>
+            )}
+            <InputGroup
+              icon={customIcons.lock}
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+             {passwordError && (
+              <Text fontWeight={'bold'} color="red.500" fontSize="sm" textAlign="left" w="full">
+                {passwordError}
+              </Text>
+            )}
+          </VStack>
+          <Button
+            mt={8}
+            w="full"
+            bg={colors.bg.accent}
+            color={colors.text.secondary}
+            _hover={{ bg: "rgba(102, 0, 51, 0.75)" }}
+            size="lg"
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+
+        <Divider my={8} borderColor={colors.text.secondary} />
+
+        <Text textAlign="center" color={colors.text.primary}>
+          Don't have an account?{" "}
+          <Link
+            to={UnAuthenticatedRoutesNames.Register}
+            fontWeight={"bold"}
+            as="span"
+            color={colors.bg.accent}
+            cursor="pointer"
+          >
+            Create one
+          </Link>
+        </Text>
+      </Box>
+    </Flex>
+  );
 };
 
 // InputGroup Component for Icon and Input Field
 const InputGroup = ({ icon, placeholder, type = "text", value, onChange }) => {
-    return (
-        <HStack
-            spacing={3}
-            w="full"
-            bg={colors.text.secondary}
-            p={3}
-            borderRadius="md"
-            boxShadow="sm"
-        >
-            <Icon as={icon} color={colors.bg.icon} />
-            <Input
-                type={type}
-                placeholder={placeholder}
-                variant="unstyled"
-                bg="transparent"
-                _placeholder={{ color: "gray.400" }}
-                focusBorderColor={colors.bg.accent}
-                value={value}
-                onChange={onChange}
-            />
-        </HStack>
-    );
+  return (
+    <HStack
+      spacing={3}
+      w="full"
+      bg={colors.text.secondary}
+      p={3}
+      borderRadius="md"
+      boxShadow="sm"
+    >
+      <Icon as={icon} color={colors.bg.icon} />
+      <Input
+        type={type}
+        placeholder={placeholder}
+        variant="unstyled"
+        bg="transparent"
+        _placeholder={{ color: "gray.400" }}
+        focusBorderColor={colors.bg.accent}
+        value={value}
+        onChange={onChange}
+      />
+    </HStack>
+  );
 };
 
 export default Login;
