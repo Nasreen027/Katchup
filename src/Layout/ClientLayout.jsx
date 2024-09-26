369fa50b0f0f3560e8e3d2debf7552d8b58bd141
Nasreen <nasreenmalik429@gmail.com>
2024-09-26 19:06:48 +0500

    Update ClientLayout.jsx

--- src/Layout/ClientLayout.jsx
+++ src/Layout/ClientLayout.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
-import ClientHeader from "../components/unauthenticated/clientHeader";
 import Footer from "../components/Footer";
 import styled from "styled-components";
+import ClientHeader from "../components/unauthenticated/ClientHeader";
 
 const MainClientComponent = styled.div`
   background-color: #e1d6c5;
